e370f7cfca390a434ed8085fc08e80d6aa30be2b
seikhchilli <69417426+seikhchilli@users.noreply.github.com>
2021-07-05 12:53:24 +0530

    delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-var cardbox = document.getElementById("cardbox");
-var button = document.getElementById("btn");
-
-button.addEventListener("click", function(){
-    cardbox.className = "card flip-effect";
-});
