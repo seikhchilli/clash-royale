a7f7362362acf86b81607c98781e8397a1062070
seikhchilli <saurabhkrsingh2001@gmail.com>
2021-07-05 10:58:19 +0530

    card update

---
+++ script.js
@@ -0,0 +1,6 @@
+var cardbox = document.getElementById("cardbox");
+var button = document.getElementById("btn");
+
+button.addEventListener("click", function(){
+    cardbox.className = "card flip-effect";
+});
